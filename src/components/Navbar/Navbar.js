08638455aa61b01608d9bedf41771bcd8372d402
Joseph-Marlitt <joseph.marlitt@gmail.com>
2018-03-27 16:41:00 -0500

    basic front end

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Navbar.css"
+const Navbar = () => 
+         <ul className= "navbar">
+                    
+                    <li className="formButton"><button>New Form</button></li>
+                    <li className="aboutUs"><button>About Us</button></li>
+                    <li className="learnMore"><button>Learn More</button></li>
+                    <li className="login"><button>Log In</button></li>
+                </ul>;
+export default Navbar
