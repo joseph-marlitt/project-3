0e70cbdbea376bfe7777d8672161c51e021d7d52
Travis Roberts <travis.roberts2@gmail.com>
2018-03-30 17:08:49 -0500

    initial dir setup, server and apartments and renters controllers and routes

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+'use strict';
+var router = require('express').Router();
+module.exports = function() {
+  router.use('/renters', require('../src/renters/route'));
+  router.use('/apartments', require('../src/apartments/route'));
+
+  // Make sure this is after all of
+  // the registered routes!
+  router.use(function (req, res) {
+    res.status(404).end();
+  });
+
+  return router;
+};
