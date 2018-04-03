75cdedec5007a23424cf8c687f617100f03795a2
Joseph-Marlitt <joseph.marlitt@gmail.com>
2018-04-03 15:27:50 -0500

    draft

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,24 +1,26 @@
 import React from "react";
-import { BrowserRouter as  Route } from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import "./Home.css"
 const Home = () => (
-    <Route render={({ history}) => (
+  <div className="homeContainer">
     <div className="buttonContainer">
       <button
+        className="homeButton"
         type='button'
-        onClick={() => { history.push('/UserForm') }}
+        onClick= {<Link to="/userform">← Users</Link>}
       >
         Renters
       </button>
       <button
-      type='button'
-      onClick={() => { history.push('/ManagerForm') }}
-        >
+        className="homeButton"
+        type='button'
+        onClick={<Link to="/">← Managers</Link>}
+      >
         Managers
-        </button>   
+        </button>
+      </div>
+      <p className= "logInHome">Already filled out a form? Log in <Link to="/"> Here</Link></p>
     </div>
-      
-    )} />
   )
 
 export default Home
