77a43ec06522984a6399951a7f2e739b4838c401
Travis Roberts <travis.roberts2@gmail.com>
2018-04-04 20:45:49 -0500

    updated renter model

--- models/Renter.js
+++ models/Renter.js
@@ -2,15 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const renterSchema = new Schema ({
-  fullName: {type: String, trim: true, max: 100, required: 'Please enter your first and last name'},
+  fullName: {type: String, trim: true, max: 100, required: true},
   title: {type:String, max: 10},
-  street: {type: String, trim: true, max: 55, required: 'Please enter address'},
-  city: {type: String, trim: true, max: 30, required: 'Please enter city name'},
+  email: {type:String, max: 100},
+  street: {type: String, trim: true, max: 55, required: true},
+  city: {type: String, trim: true, max: 30, required: true},
   zip: {type: Number, trim: true, required: 'Please enter zipcode'},
-  state: {type: String, trim: true, max: 50, required: 'Please enter state'},
+  state: {type: String, trim: true, max: 50, required: true},
+  minBeds: {type: Number, required: true},
+  minBath: {type: Number, required: true},
+  maxRent: {type: Number, required: true},
+  creditrating: {type: String, required: true},
   contactInfo: {type: Array, "default": [] },
-  pets: {type: Array, "default": [] },
-  beds: {type: Array, "default": [] },
+  pets: {type: Boolean, required: true },
   date: {type: Date, default: Date.now}
 });
 
